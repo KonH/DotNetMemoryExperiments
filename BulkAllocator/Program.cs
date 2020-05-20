6a696a865299038b3eb78c859a1ce092c3843b64
KonH <konh@yandex.ru>
2020-05-20 19:01:07 +0700

    In-process memory monitoring, additional step to use allocated memory

--- BulkAllocator/Program.cs
+++ BulkAllocator/Program.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 
 namespace BulkAllocator {
 	class Program {
+		const int PageSize = 4048;
+
 		static LinkedList<byte[]> Arrays = null;
 
 		static void Main(string[] args) {
@@ -17,23 +20,49 @@ namespace BulkAllocator {
 			var iterationCount = int.Parse(args[2]);
 			var iterationGap = TimeSpan.FromSeconds(int.Parse(args[3]));
 			var idleTime = TimeSpan.FromSeconds(int.Parse(args[4]));
-
+			var process = Process.GetCurrentProcess();
 			while ( true ) {
+				process.Refresh();
+				var workingSetAtStart = process.WorkingSet64;
+				var privateMemorySizeAtStart = process.PrivateMemorySize64;
 				Arrays = new LinkedList<byte[]>();
 				for ( var i = 0; i < iterationCount; i++ ) {
-					Console.WriteLine($"Iteration {i}/{iterationCount - 1}: allocate {bytesToAllocate}x{arrayCountPerIteration} bytes");
+					Console.WriteLine($"Allocate Iteration {i}/{iterationCount - 1}: allocate {bytesToAllocate}x{arrayCountPerIteration} bytes");
 					for ( var j = 0; j < arrayCountPerIteration; j++ ) {
 						Arrays.AddLast(new byte[bytesToAllocate]);
 					}
-					Console.WriteLine($"Iteration: Waiting for ~{iterationGap.TotalSeconds} sec");
+					Console.WriteLine($"Allocate Iteration: Waiting for ~{iterationGap.TotalSeconds} sec");
 					Thread.Sleep(iterationGap);
 				}
+				ShowMemoryDiff(process, workingSetAtStart, privateMemorySizeAtStart);
+				for ( var i = 0; i < iterationCount; i++ ) {
+					Console.WriteLine($"Use Iteration {i}/{iterationCount - 1}: access allocated bytes");
+					foreach ( var array in Arrays ) {
+						for ( var j = 0; j < bytesToAllocate; j += PageSize ) {
+							array[j] = 1;
+						}
+					}
+					Console.WriteLine($"Use Iteration: Waiting for ~{iterationGap.TotalSeconds} sec");
+					Thread.Sleep(iterationGap);
+				}
+				ShowMemoryDiff(process, workingSetAtStart, privateMemorySizeAtStart);
 				Arrays = null;
 				Console.WriteLine("GC.Collect()");
 				GC.Collect();
 				Console.WriteLine($"Idle: Waiting for ~{idleTime.TotalSeconds} sec");
+				ShowMemoryDiff(process, workingSetAtStart, privateMemorySizeAtStart);
 				Thread.Sleep(idleTime);
+				Console.WriteLine();
 			}
+		}
+
+		static void ShowMemoryDiff(Process process, long workingSetAtStart, long privateMemorySizeAtStart) {
+			process.Refresh();
+			var workingSetAfter = process.WorkingSet64;
+			var privateMemorySizeAfter = process.PrivateMemorySize64;
+			var workingSetDiffAfter = (workingSetAfter - workingSetAtStart);
+			var privateMemorySizeDiffAfter = (privateMemorySizeAfter - privateMemorySizeAtStart);
+			Console.WriteLine($"WorkingSetDiffAfter: {workingSetDiffAfter}, PrivateMemorySizeDiffAfter: {privateMemorySizeDiffAfter}");
 		}
 	}
 }
